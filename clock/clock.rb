6194e7816e6687fd77693792840d32e7f16dc0ae
Jared Echols <jechols@mavenlink.com>
2018-11-16 15:35:07 -0700

    Made some methods private and removed attr_reader in clock

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,24 +1,48 @@
 class Clock
   def initialize(hours, minutes)
-    @hours = hours
-    @minutes = minutes
+    @hours = calculate_hours(hours, minutes)
+    @minutes = calculate_minutes(minutes)
+  end
+
+  def +(addend_in_minutes)
+    Clock.new(
+      calculate_hours(@hours, @minutes + addend_in_minutes),
+      calculate_minutes(@minutes + addend_in_minutes)
+    )
+  end
+
+  def ==(other)
+    to_s == other.to_s
+  end
+
+  def to_s
+    hours_string = format_two_digits(@hours)
+    minutes_string = format_two_digits(@minutes)
+    "#{hours_string}:#{minutes_string}"
+  end
+
+  private
+
+  def calculate_hours(hours, minutes)
+    hours_from_extra_minutes = minutes / 60
+    (hours + hours_from_extra_minutes) % 24
+  end
+
+  def calculate_minutes(minutes)
+    minutes % 60
   end
 
   def self.at(hours, minutes)
     Clock.new(hours, minutes)
   end
 
-  def lead_with_zero(num)
+  def format_two_digits(num)
+    return "00" if num == 0
+
     num < 10 ? "0#{num}" : "#{num}"
   end
+end
 
-  def to_s
-    hours_string = lead_with_zero(hours)
-    minutes_string = lead_with_zero(minutes)
-    "#{hours_string}:#{minutes_string}"
-  end
-
-  private
-
-  attr_reader :hours, :minutes
+module BookKeeping
+  VERSION = 2 # Where the version number matches the one in the test.
 end
