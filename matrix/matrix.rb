ecfdcfb8bec860a7b962c656881b88901b824435
Jared Echols <jechols@mavenlink.com>
2019-04-07 11:23:39 -0600

    refactor

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,23 +1,19 @@
 class Matrix
   def initialize(input)
-    parse(input)
-  end
-
-  def parse(input)
-    @parsed_array = input.strip.split(/\n/).map do |row|
+    @matrix = input.strip.split(/\n/).map do |row|
       row.strip.split(/\s/).map(&:to_i)
     end
   end
 
   def rows
-    parsed_array
+    matrix
   end
 
   def columns
-    parsed_array.transpose
+    matrix.transpose
   end
 
   private
 
-  attr_reader :parsed_array
+  attr_reader :matrix
 end
