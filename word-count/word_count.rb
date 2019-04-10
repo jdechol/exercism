60489d176eea1308ffe5368e7fe063844ce7a6e0
Jared Echols <jechols@mavenlink.com>
2019-04-10 06:07:26 -0600

    refactor word_count for readability

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,10 +1,19 @@
 class Phrase
-  attr_reader :word_count
+  def initialize(sentence)
+    @sentence = sentence
+  end
 
-  def initialize(words)
-    @word_count = Hash.new(0)
-    words.strip.scan(/\b['\w]+\b/).each do |word|
-      word_count[word.downcase] += 1
+  def word_count
+    words_in(sentence).each_with_object(Hash.new(0)) do |word, counts|
+      counts[word.downcase] += 1
     end
   end
+
+  private
+
+  def words_in(sentence)
+    sentence.strip.scan(/\b['\w]+\b/)
+  end
+
+  attr_reader :sentence
 end
