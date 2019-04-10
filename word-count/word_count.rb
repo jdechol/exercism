5ad0cf481efc0f4a708d4563f76f65ee18356182
Jared Echols <jechols@mavenlink.com>
2019-04-10 05:54:59 -0600

    refactor word_count to use scan with word boundry

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -3,17 +3,8 @@ class Phrase
 
   def initialize(words)
     @word_count = Hash.new(0)
-    words.strip.split(/[^'\w]+/).each do |word|
-      word_count[strip(word.downcase)] += 1
+    words.strip.scan(/\b['\w]+\b/).each do |word|
+      word_count[word.downcase] += 1
     end
-  end
-
-  private
-
-  def strip(word)
-    result = word
-    result.slice!(0) if result[0] == "'" || result[0] == '"'
-    result.slice!(result.length-1) if result[result.length - 1] == "'" || result[result.length - 1] == '"'
-    result
   end
 end
